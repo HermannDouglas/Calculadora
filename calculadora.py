9151432c21baf30de37499c79093e2f0b8774edf
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2022-08-09 19:56:14 -0500

    Segundo commit

--- calculadora.py
+++ calculadora.py
@@ -3,3 +3,12 @@ number_2 = int(input('Enter your second number: '))
 
 print('{} + {} = '.format(number_1, number_2))
 print(number_1 + number_2)
+
+print('{} - {} = '.format(number_1, number_2))
+print(number_1 - number_2)
+
+print('{} * {} = '.format(number_1, number_2))
+print(number_1 * number_2)
+
+print('{} * {} = '.format(number_1, number_2))
+print(number_1 + number_2)
